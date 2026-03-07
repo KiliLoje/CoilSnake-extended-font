00aa36921ea85f70dc25f93633506765ce844317
KiliLoje <salaun.mael09@gmail.com>
2026-03-07 19:10:39 +0100

    add patch() to coilsnake/util/eb/helper.py

--- coilsnake/util/eb/helper.py
+++ coilsnake/util/eb/helper.py
@@ -1,6 +1,12 @@
+from coilsnake.util.eb.pointer import from_snes_address
+
 def is_in_bank(bank, address):
     return (address >> 16) == bank
 
 
 def not_in_bank(bank, address):
     return not is_in_bank(bank, address)
+
+
+def patch(rom, size, offset, instructions):
+    rom[from_snes_address(offset) : from_snes_address(offset + size)] = instructions
